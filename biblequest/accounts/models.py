3660ef64cfc5d87814262a7be072c167b6fb3438
Kandukurisanghavi <Sanghavi.kandukuri@unh.edu>
2025-03-21 21:08:44 -0400

    Add Comment model to link user comments with prayer requests and enhance prayer request functionality

--- biblequest/accounts/models.py
+++ biblequest/accounts/models.py
@@ -1,5 +1,5 @@
-# accounts/models.py
 from django.db import models
+from django.contrib.auth.models import User
 
 class QuizQuestion(models.Model):
     DIFFICULTY_CHOICES = [
@@ -25,3 +25,20 @@ class VerseOfTheDay(models.Model):
 
     def __str__(self):
         return f"{self.reference} - {self.text}"
+
+class PrayerRequest(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.text[:50]}"
+
+class Comment(models.Model):
+    prayer_request = models.ForeignKey(PrayerRequest, related_name='comments', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.text[:50]}"
